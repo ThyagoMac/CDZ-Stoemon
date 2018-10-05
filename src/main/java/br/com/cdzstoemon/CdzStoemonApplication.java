df6de496904252ee86b90118976166b0f223eb21
thyago.cormick <thyago.cormick@gmail.com>
2018-10-05 15:22:27 -0300

    Starting

---
+++ src/main/java/br/com/cdzstoemon/CdzStoemonApplication.java
@@ -0,0 +1,12 @@
+package br.com.cdzstoemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdzStoemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdzStoemonApplication.class, args);
+	}
+}
